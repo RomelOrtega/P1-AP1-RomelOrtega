ee6a9dc1d3c859037e50a4d979fb3329042008dc
RomelOrtega <romel_ortega@ucne.edu.do>
2025-09-22 20:33:14 -0400

    Se creo el service

---
+++ Services/Service.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using P1_AP_RomelOrtega.Models;
+using P1_AP1_RomelOrtega.DAL;
+
+namespace P1_AP1_RomelOrtega.Services
+{
+    public class Service(IDbContextFactory<Contexto> DbFactory)
+    {
+        public async Task<List<Registro>> Listar(Expression<Func<Registro, bool>> criterio)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Registros
+                .AsNoTracking()
+                .Where(criterio)
+                .ToListAsync();
+        }
+    }
+}
