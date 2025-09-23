22c006030311c8655751f7ec5ebd1fb4dde109a0
RomelOrtega <romel_ortega@ucne.edu.do>
2025-09-22 20:33:39 -0400

    Se creo el contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P1_AP_RomelOrtega.Models;
+
+namespace P1_AP1_RomelOrtega.DAL;
+
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<Registro> Registros { get; set; }
+    }
+
